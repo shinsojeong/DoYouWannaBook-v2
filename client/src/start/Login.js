3ab793c7ff6b0edea5ba4a16b4978cf2eaafac83
shinsojeong <shinsojeong82@gmail.com>
2021-05-27 20:45:15 +0900

    210527 change structure of folder

--- client/src/start/Login.js
+++ client/src/start/Login.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+
+    const login = () => {
+        
+    };
+
     return (
         <div id="login">
             <form>
-                <input type="text" />
-                <input type="password" />
+                <input type="text" id="id" onChange={(e) => setId(e.target.value)}/>
+                <input type="password" id="pw" onChange={(e) => setId(e.target.value)}/>
             </form>
         </div>
     );
