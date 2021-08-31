3b056edd45726df1f44a6633a4c14be0b12692c9
shinsojeong <shinsojeong82@gmail.com>
2021-09-01 08:26:25 +0900

    210901 footer menu 구현

---
+++ src/common/util/Menu.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { AiOutlineHome, AiOutlineBarcode, AiOutlineShareAlt, AiOutlineMessage, AiOutlineUser } from "react-icons/ai";
+import '../../styles/menu.scss';
+
+const TopBar = () => {
+    const history = useHistory();
+
+    //function
+    const go = (url) => {
+        history.push(`${url}`);
+    };
+
+    return (
+        <footer className="foot_menu">
+            <table>
+                <tbody>
+                    <tr id="menu_items">
+                        <td><AiOutlineBarcode className="icon" size="50" onClick={() => go("/user/barcode")}/></td>
+                        <td><AiOutlineShareAlt className="icon" size="50" onClick={() => go("/user/std-main")}/></td>
+                        <td><AiOutlineHome className="icon" size="50" onClick={() => go("/user/home")}/></td>
+                        <td><AiOutlineMessage className="icon" size="50" onClick={() => go("/user/chat-list")}/></td>
+                        <td><AiOutlineUser className="icon" size="50" onClick={() => go("/user/mypage")}/></td>
+                    </tr>
+                    <tr id="title">
+                        <td>바코드 대출</td>
+                        <td>공유 도서</td>
+                        <td>홈</td>
+                        <td>채팅</td>
+                        <td>마이페이지</td>
+                    </tr>
+                    </tbody>
+            </table>
+        </footer>
+    );
+};
+
+export default TopBar;
